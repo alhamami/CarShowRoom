91415fa1f02936fe759d0521414c3e2b03e2a0c3
alhamami <jalalalhamami@gmail.com>
2024-11-10 15:39:26 -0800

    Backend setup of Car Show Room project

---
+++ Backend/src/main/java/com/elm/carshowrooms/CarshowroomsApplication.java
@@ -0,0 +1,13 @@
+package com.elm.carshowrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarshowroomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarshowroomsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/elm/carshowrooms/CarshowroomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elm.carshowrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarshowroomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
